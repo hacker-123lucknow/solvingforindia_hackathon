97262440d5dcf48b9b0bd56c94d2adbc6e0286c4
UMAKANT <umakantsingh900@gmail.com>
2023-04-15 18:36:42 +0530

    hackathon project

---
+++ app/src/main/java/com/example/THRIFTy/IncomeItems.java
@@ -0,0 +1,35 @@
+package com.example.THRIFTy;
+
+public class
+IncomeItems {
+    private int id;
+    private int spinnerimage;
+    private String spinnertext;
+
+    public IncomeItems(int id,int spinnerimage, String spinnertext) {
+        this.id = id;
+        this.spinnerimage = spinnerimage;
+        this.spinnertext = spinnertext;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSpinnerimage() {
+        return spinnerimage;
+    }
+    public void setSpinnerimage(int spinnerimage) {
+        this.spinnerimage = spinnerimage;
+    }
+    public String getSpinnertext() {
+        return spinnertext;
+    }
+    public void setSpinnertext(String spinnertext) {
+        this.spinnertext = spinnertext;
+    }
+}
